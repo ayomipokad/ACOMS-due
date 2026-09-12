109236c52eca8bef0f6950813e87ec7711f2c850
ayomipokad <ayomipokatola@gmail.com>
2026-09-12 13:31:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // ------------------------------------------------------------------
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://qsktesfizdmfbsmpdxud.supabase.co/rest/v1/'; // e.g. https://xxxxx.supabase.co
+const SUPABASE_URL = 'https://qsktesfizdmfbsmpdxud.supabase.co'; // e.g. https://xxxxx.supabase.co
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFza3Rlc2ZpemRtZmJzbXBkeHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMDEzMzksImV4cCI6MjEwNDc3NzMzOX0.uSTuqs6xhxhEaXwYSOAT1hpMExaDGLi63403TGQgoec';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
